fb4e5551e8a4c9f12ef1a5db24c62cf5666b79c5
hiisuuii <17937620+hiisuuii@users.noreply.github.com>
2025-06-12 23:52:59 -0600

    update this to rig the event chance

--- src/main/java/live/hisui/tbspatch/command/ForceEventCommand.java
+++ src/main/java/live/hisui/tbspatch/command/ForceEventCommand.java
@@ -24,6 +24,7 @@ public class ForceEventCommand {
                             var id = IntegerArgumentType.getInteger(ctx, "id");
                             TBSPatchMod.forcedEventID.set(id);
                             TBSPatchMod.LOGGER.info("We are attempting to force an event with ID: {}",TBSPatchMod.forcedEventID.get());
+                            TBSPatchMod.riggedEventChance.set(true);
                             EventEngineProcedure.execute(ctx.getSource().getLevel(), ctx.getSource().getPosition().x, ctx.getSource().getPosition().y, ctx.getSource().getPosition().z, ctx.getSource().getEntityOrException());
                             return IntegerArgumentType.getInteger(ctx, "id");
                         }))
